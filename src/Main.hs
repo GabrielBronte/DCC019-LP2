e2cbfa0778dc52ccc6918b24184fd3b8cacdf96a
Gabriel Bronte <gabriel.bronte@levty.com>
2022-07-06 12:56:59 -0300

    Finalizando Criptogame

--- src/Main.hs
+++ src/Main.hs
@@ -7,26 +7,26 @@ import System.Exit (exitSuccess)
 
 main :: IO ()
 main = do
-    playGame 0
-
-playGame :: Int -> IO ()
-playGame counter = do
     putStrLn "\n\nBem vindo ao CriptoGame!"
-    code <- getRandomCodes
-    putStrLn $ show code
     putStrLn $ "\nO computador gerou um código de 4 dígitos."
     putStrLn $ "Cada um destes dígitos é um valor entre 1 e 6."
     putStrLn $ "Adivinhe o código gerado no menor número de turnos possíveis.\n"
-    forever $ do
-        guessString <- prompt "? "
-        case parseCodeGuess guessString 4 of
-            Just guess -> do
-                let counter = (counter+1)
-                let feedback = getFeedback code guess
-                printFeedback feedback
-                when (winnerCondition code guess) $ do
-                    putStrLn $ "Correto! Voce desvendou o código secreto em " ++ show counter ++ " movimentos!"
-                    exitSuccess
-            Nothing -> do
-                putStrLn "Não foi possível reconhecer sua entrada, tente novamente."
-                return ()
+    code <- getRandomCodes
+    putStrLn $ show code
+    playGame 1 code
+
+playGame :: Int -> Code -> IO ()
+playGame counter code = do
+    guessString <- prompt "? "
+    case parseCodeGuess guessString 4 of
+        Just guess -> do
+            let feedback = getFeedback code guess
+            printFeedback feedback
+            if (winnerCondition code guess)
+            then do
+                putStrLn $ "Correto! Voce desvendou o código secreto em " ++ show counter ++ " movimentos! \n\n"
+                exitSuccess
+            else playGame (counter+1) code
+        Nothing -> do
+            putStrLn "Não foi possível reconhecer sua entrada, tente novamente."
+            playGame (counter+1) code
