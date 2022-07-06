a8761fcb74eacf3c2d0b9c3aadec10babf7b93aa
Gabriel Bronte <gabriel.bronte@levty.com>
2022-07-06 15:41:37 -0300

    Finalizando CriptoGame

--- src/CompActions.hs
+++ src/CompActions.hs
@@ -1,4 +1,4 @@
-module CompActions (getRandomCodes, getFeedback, winnerCondition, removeCorrectPlaces) where
+module CompActions (getRandomCodes, getFeedback, winnerCondition, calcGame, select_fst, select_snd, select_trd) where
 import Types
 import System.Random (randomRIO)
 import Control.Applicative ((<$>))
@@ -21,21 +21,29 @@ getFeedback :: Code -> Guess -> Feedback
 getFeedback (Code c) (Guess g) =
     Feedback $ show numCorrectPlace ++ " Completo, " ++ show numCorrectCodeButNotPlace ++ " Parcial "
     where
-        numCorrectPlace = length . filter id $ zipWith (==) c g 
-        codeGuessWithoutDuplicates = removeCorrectPlaces c g
-        numCorrectCodeButNotPlace = length $ intersect (fst codeGuessWithoutDuplicates) (snd codeGuessWithoutDuplicates)
+        feedback = (calcGame c g) 
+        numCorrectPlace = select_fst (feedback) 
+        numCorrectCodeButNotPlace = length $ intersect (select_snd (feedback) ) (select_trd (feedback) )
 
 -- | Se o palpite corresponde ao código.
 winnerCondition :: Code -> Guess -> Bool
 winnerCondition (Code c) (Guess g) = c == g
 
--- | Compara o palpite com o código e remove os códigos completos
-removeCorrectPlaces :: Eq a => [a] -> [a] -> ([a], [a])
-removeCorrectPlaces [] [] = ( [], [] )
-removeCorrectPlaces (hh:ht) (gh:gt)
-    | hh == gh = (hl, gl)
-    | otherwise = (hh:hl, gh:gl)
-    where (hl, gl) = removeCorrectPlaces ht gt
+-- | Retorna quantidade de codigos completos e os removem do palpite e do segredo
+calcGame :: Eq a => [a] -> [a] -> (Int, [a], [a])
+calcGame [] [] = ( 0, [], [] )
+calcGame (hh:ht) (gh:gt)
+    | hh == gh = ( c+1, hl, gl )
+    | otherwise = ( c, hh:hl, gh:gl )
+    where ( c, hl, gl ) = calcGame ht gt
 
+select_fst ::Eq a => (Int, [a], [a]) -> Int
+select_fst(x, _, _) = x
+
+select_snd ::Eq a => (Int, [a], [a]) -> [a]
+select_snd(_, y, _) = y
+
+select_trd :: (Int, [a], [a]) -> [a]
+select_trd(_, _, z) = z
 
 
